de2a2aa7f69074c6945a17427b0b80c91a50904c
mashahabi15 <ma.shahabi15@gmail.com>
2023-03-02 09:15:12 +0330

    initial commit

---
+++ drafts.py
@@ -0,0 +1,133 @@
+# pi = 3.14  # float
+# print(pi)
+# ten = 10  # int
+# double_pi = pi * 2
+# # print(double_pi)
+# # hiiiiiiiiiiiiiiiii
+# """
+# hiiiiiiii
+# how are you today?
+# I'm fine!
+# """
+# pi = "I am a woman"  # String
+# print(pi)
+
+# name = 'Jamila'
+# age = 45
+# my_var = 8.76
+# print(name, age)
+# print("Jamila\n45")
+# info = 'I\'m coming'
+# info2 = "You are going to \"home\"!"
+# info3 = "The \\sound is loud!"
+# print(info)
+# print(info2)
+# print(info3)
+
+# invoke فراخوانی
+# import math
+# print(math.fabs(-3)) # float absolute
+
+# import sys  # system
+
+# print(sys.version)
+
+# print("Enter something...")
+# my_inp = input()
+# print(my_inp)
+
+# print("Enter your name:")
+# your_name = input()
+# print(your_name)
+
+# 1. Concatenation
+# print("Your name is " + your_name + " !!!")
+# # 2. format()
+# print("Your name is {} !!!".format(your_name))
+# # 3. f string
+# print(f"Your name is {your_name} !!!")
+
+# name = input("Enter your name:")
+# age = input("Enter your age:")
+# print(f"Your name is {name} and you are {age} y.o. !!")
+
+# my_age = 24  # ineger
+# print(type(my_age))
+# my_name = "Maedeh"  # string
+# print(type(my_name))
+# pi = 3.14  # float
+# print(type(pi))
+# is_young = True  # boolean (True/False)
+# print(type(is_young))
+
+# Type Casting
+# age = input("Enter your age:")
+# print(type(age))
+# age = int(age)
+# print(type(age))
+
+# age = int(input("Enter your age:"))
+# print(type(age))
+
+# num1 = int(input("Enter a number:\t"))
+# num2 = int(input("Enter another number:\t"))
+# print(num1 + num2)
+# result = num1 + num2
+# print(result)
+
+# birth_year = int(input("Enter your birth year:\t"))
+# age = 2023 - birth_year
+# print(f"You are {age} y.o.")
+
+# Type Casting with detail
+# my_num = float(input("Enter a floating point number:\t"))
+# print(f"input number is {my_num} and the type is {type(my_num)}")
+# print(int(my_num))
+
+# print(int("8"))
+# print(float("14.702"))
+# print(int(14.702))
+
+# my_num = input("Enter a floating point number")
+# my_num = float(my_num)
+# doubled_my_num = my_num * 2
+# print(doubled_my_num)
+
+# Strings
+# my_str = "I'm going to my uncle's house"
+# my_str2 = 'Im going to my uncles house'
+# my_str = "Hello worlD! wooow""Hello worlD! wooow"
+#
+# # upper()
+# print(f"upper() -> {my_str.upper()}")
+#
+# # lower()
+# print(f"lower() -> {my_str.lower()}")
+#
+# # title()
+# print(f"title() -> {my_str.title()}")
+#
+# # swapcase()
+# print(f"swapcase() -> {my_str.swapcase()}")
+
+# my_str = my_str.upper()
+
+# print(my_str)
+
+# my_str2 = "Kabul"  # length -> 5 - case sensitive
+# print(len(my_str2))
+#
+# print(my_str2.startswith("ka"))
+#
+# print(my_str.endswith("oW"))
+
+
+# my_str3 = "12ukijuh34"
+# my_str4 = "1234"
+# my_str5 = "hello"
+# print(f"isnumeric() -> {my_str4.isnumeric()}")
+# print(f"isalpha() -> {my_str5.isalpha()}")
+# print(f"isalnum() -> {my_str3.isalnum()}")
+
+my_str = "World"  # index
+print(my_str[5])
